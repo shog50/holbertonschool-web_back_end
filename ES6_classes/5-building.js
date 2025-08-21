2e70d77b69a698ad85cac3259cbade2d7b81d16c
shog50 <10709@holbertonstudents.com>
2025-08-21 20:46:54 +0000

    A Building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,21 @@
-export default class Building {
+class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-
-  
-    if (this.constructor !== Building && 
-        typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
+
+    this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 }
+
+export default Building;
 
