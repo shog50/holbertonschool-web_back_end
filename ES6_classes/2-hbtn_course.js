b47af3044cbac9987b1b65bf072d1e1a480846ac
shog50 <10709@holbertonstudents.com>
2025-08-21 21:12:00 +0000

    A Course, Getters, and Setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -5,43 +5,47 @@ class HolbertonCourse {
     this.students = students;
   }
 
+  /**
+   * @param {String} name
+   */
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
   get name() {
     return this._name;
   }
 
-  set name(name) {
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
-      throw new TypeError('Name must be a string');
+  /**
+   * @param {Number} length
+   */
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = length;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(length) {
-    if (typeof length === 'number') {
-      this._length = length;
+  /**
+   * @param {Array} students
+   */
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
     } else {
-      throw new TypeError('Length must be a number');
+      throw new TypeError('Students must be an Array');
     }
   }
 
   get students() {
     return this._students;
-  }
-
-  set students(students) {
-    if (
-      Array.isArray(students)
-      && students.every((s) => typeof s === 'string')
-    ) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students must be an array of strings');
-    }
   }
 }
 
