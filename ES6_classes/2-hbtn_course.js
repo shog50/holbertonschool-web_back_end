f757beb1f9fe12862952f62e1db5facf07d98cbd
shog50 <10709@holbertonstudents.com>
2025-08-21 21:14:34 +0000

    A Course, Getters, and Setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,53 +1,51 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
-  }
-
-  /**
-   * @param {String} name
-   */
-  set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
     this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  /**
-   * @param {Number} length
-   */
-  set length(length) {
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    this._length = length;
+    this._name = value;
   }
 
   get length() {
     return this._length;
   }
 
-  /**
-   * @param {Array} students
-   */
-  set students(students) {
-    if (students instanceof Array) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students must be an Array');
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = value;
   }
 
   get students() {
     return this._students;
   }
-}
 
-export default HolbertonCourse;
+  set students(value) {
+    if (!Array.isArray(value) || value.some((student) => typeof student !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+}
 
