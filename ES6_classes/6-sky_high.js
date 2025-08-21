cf5be043ff7183305b3363b69d6b1f2056e8553e
shog50 <10709@holbertonstudents.com>
2025-08-21 20:49:10 +0000

    Inheritance

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // تمرير sqft للأب
+    this._floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override evacuationWarningMessage
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
+
