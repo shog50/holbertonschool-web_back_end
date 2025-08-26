18ab5e7d56183cd522fdbebdae5ee9f9dc7c85b4
shog50 <10709@holbertonstudents.com>
2025-08-26 08:25:25 +0000

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (!startString) return '';
   return [...set]
-    .filter((value) => value.startsWith(startString) && startString !== '')
+    .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
 }
