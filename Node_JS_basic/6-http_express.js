eb202dc49295406742b8f2d9828bce94b0d1f7b7
shog50 <10709@holbertonstudents.com>
2025-09-03 13:03:35 +0000

    Create a small HTTP server using Express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening in port ${port}`);
+});
+
+module.exports = app;
